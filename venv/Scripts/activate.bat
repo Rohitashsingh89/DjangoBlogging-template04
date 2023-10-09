a173eaa96921fc1f5e45ea605820b81ef43680a9
rohitashsingh89 <rohitashmtr2002@gmail.com>
2023-10-09 19:48:20 +0530

    Add requirements.txt

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\desktop\shareX\venv
+set VIRTUAL_ENV=D:\desktop\RSBlog\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
